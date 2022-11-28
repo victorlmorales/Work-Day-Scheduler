0e1f752bc667d2b484d1ae528dcaa2aba1e86171
victorlmorales <victor.l.morales2020@gmail.com>
2022-11-28 01:37:56 -0600

    Added a save on click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 //Display Today's Date at the Top
+//Displays current date at the top of the page
 var todayDate = dayjs().format('dddd, MMMM DD, YYYY');
 $('#currentDay').text(todayDate);
     
+$(function () {
+    //Saves on Click Event
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var eventText = $(this).siblings(".description").val();
+        var eventTime = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(eventTime, eventText);
+    });
+
+
+});
